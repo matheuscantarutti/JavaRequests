d1afaf00b6fa6ad2336c6925394d22aa81ba2bc3
MatheusABC <matheuscantarutti@gmail.com>
2021-03-27 15:20:57 -0300

    exe-3: validação do cpf via AJAX

---
+++ exercicio-3/exercicio3/src/main/java/com/mycompany/exercicio3/Models/Pessoa.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.exercicio3.Models;
+
+/**
+ *
+ * @author Matheus
+ */
+public class Pessoa {
+    private String cpf;
+    private String sexo;
+    private String corCabelo;
+    private String corOlhos;
+    private int idade;
+}
